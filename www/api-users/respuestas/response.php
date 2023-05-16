40fe2351b47addcdd105a40c9d51c1cbaf442187
Lorenzo Molina Moreno <lmolinamoreno@hotmail.com>
2023-05-16 18:55:56 +0200

    header para cors y https

--- www/api-users/respuestas/response.php
+++ www/api-users/respuestas/response.php
@@ -3,7 +3,8 @@ class Response
 {
 	public static function result($code, $response){
 
-		header('Content-type: application/json');
+		header('Access-Control-Allow-Origin: *');
+		header('Content-Type: application/json');
 		http_response_code($code);
 
 		echo json_encode($response);  //escribir la respuesta en json
